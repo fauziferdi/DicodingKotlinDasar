1eff32aec815650de2112200c6e5bb45223e42aa
Fauzi Ferdiansyah <fauziferdiansyahindo@gmail.com>
2025-01-06 22:19:31 +0700

    first commit

---
+++ app/src/main/java/com/example/dicodingkotlindasar/classKotlin/Main.kt
@@ -0,0 +1,16 @@
+package com.example.dicodingkotlindasar.classKotlin
+
+fun main() {
+    val dicodingCat = Animal()
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}" )
+    dicodingCat.eat()
+    dicodingCat.sleep()
+
+    // Mengubah objek dari class dicodingCat
+    dicodingCat.name = "Kucing Oren"
+    dicodingCat.weight = 6.0
+    dicodingCat.age = 3
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}" )
+    dicodingCat.eat()
+    dicodingCat.sleep()
+}
